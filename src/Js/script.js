9e7843a2fec87ce6a618ab820900b14040780314
Marcos Pedro de Macêdo <marcospedromdmacedo@outlook.com>
2025-03-23 18:36:45 -0300

    Adição do Carossel de iamgens e correção do estilo

---
+++ src/Js/script.js
@@ -0,0 +1,8 @@
+const myCarouselElement = document.getElementById("myCarousel")
+
+
+const Carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 1000,
+  touch: false,
+  pause: "hover"
+})
